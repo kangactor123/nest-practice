4723d13dab7663f77c9b80589199677098978714
강동희 <kangactor123@naver.com>
2023-12-24 15:07:48 +0900

    Config module 주입

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { MoviesModule } from './movies/movies.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeormConfig } from './configs/typeorm.config';
 import { DataSource, DataSourceOptions } from 'typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 //root module
 @Module({
@@ -13,6 +15,16 @@ import { DataSource, DataSourceOptions } from 'typeorm';
       dataSourceFactory: async (options: DataSourceOptions) => {
         return new DataSource(options).initialize();
       },
+    }),
+    // 설정 파일 관리 모듈
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env`,
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_USER: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+      }),
     }),
     MoviesModule,
   ],
--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
 
 @Injectable()
 export class TypeormConfig implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       type: 'postgres',
-      host: 'localhost',
+      host: this.configService.get<string>('DB_HOST'),
       port: 5432,
-      username: 'postgres',
-      password: 'postgres',
+      username: this.configService.get<string>('DB_USER'),
+      password: this.configService.get<string>('DB_PASSWORD'),
       database: 'postgres',
       synchronize: true, // 스키마 자동 동기화
       dropSchema: false, // 애플리케이션 실행 시 기존 스키마 삭제 여부
